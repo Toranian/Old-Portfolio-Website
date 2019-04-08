1a045db0436be078bba2a6c0ea53e0892f10ad44
9921459 <mediamanmorrow@gmail.com>
2019-04-08 13:39:13 -0700

    Started mobile development

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-function toggleNav() {
-  var element = document.getElementById("navbar");
-  if (element.style.display === "none") {
-    element.style.display = "block";
-    // element.style.transform = "translateX(100px)"
-  } 
-  else {
-    element.style.display = "none";
-    // element.style.transform = "translateX(-100px)"
+// function toggleNav() {
+//   var element = document.getElementById("navbar");
+//   if (element.style.display === "none") {
+//     element.style.display = "block";
+//     // element.style.transform = "translateX(100px)"
+//   }
+//   else {
+//     element.style.display = "none";
+//     // element.style.transform = "translateX(-100px)"
+//
+//   }
+// }
 
-  }
+function toggleNav(element) {
+  element.classList.toggle("change");
+  navbar = document.classList.toggle("movenav")
+  console.log("clicked.")
 }
+
+// console.log("Large display settings.")
