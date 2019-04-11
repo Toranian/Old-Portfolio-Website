ed56a1b6798f92c8273dc21dda56c2b0846b4315
9921459 <mediamanmorrow@gmail.com>
2019-04-11 12:08:00 -0700

    Added jQuery to change the navbar

--- js/main.js
+++ js/main.js
@@ -10,26 +10,75 @@
 //
 //   }
 // }
-
 function toggleNav() {
-  var navbar = document.querySelector("#navbar")
-  var menu = document.querySelector("#menu")
-  var backgroundimage = document.querySelector("#backgroundimage")
+  var navbar = document.querySelector("#navbar");
+  var menu = document.querySelector("#menu");
+  var backgroundimage = document.querySelector("#backgroundimage");
 
   // backgroundimage.classList.toggle("blur")
   menu.classList.toggle("change");
-  navbar.classList.toggle("movenav")
-  console.log("clicked.")
+  navbar.classList.toggle("movenav");
 }
 
+$(document).scroll(function() {
+    var currentscroll = $(document).scrollTop();
+    var distance = $("#about").offset().top;
+    if (currentscroll > distance) {
+      $(".nav-link").css({
+        "border-bottom":"2px solid black",
+        "border-top":"none",
+        "color":"black",
+      })
+
+      $(".nav-link:hover").css({
+        "background-color":"rgba(0, 0, 0, 0.3)"
+      })
+
+      $("#chevron").css({
+        "border-right":"2px solid black"
+      })
+    }
+
+    else {
+      $(".nav-link").css({
+        "border-bottom":"2px solid white",
+        "border-top":"2px solid white",
+        "color":"white"
+      })
+
+      $("#chevron").css({
+        "border-right":"2px solid black"
+      })
+
+
+    }
+
+})
+
+
+// $(function() {
+// });
+
 // $(document).scroll(function() {
-//   var progress = ($(document).scrollTop());
-//   if (progess >= 1080) {
-//     document.querySelector("nav ul li a").style = {
-//       color: black;
-//     }
+//   var progress = $(document).scrollTop();
+//   if (progress >= 1080) {
+//     $("nav-link").css("border-bottom":"2px black solid");
+//     console.log("past 1080pixels");
 //   }
+//   console.log(progress)
 // })
+
+
+// $(document).scroll(function() {
+//     console.log($(document).scrollTop());
+// })
+
+// var viewableOffset = $("#home").offset().top - $(window).scrollTop();
+
+// var distance = $("#home").offset().top;
+// console.log(distance)
+
+
 
 // $('a[href^="#"]').on('click', function (e) {
 //   e.preventDefault();
@@ -44,9 +93,11 @@ function toggleNav() {
 //   });
 // });
 
-$('#home').click(function () {
-  $("html, body").animate({ scrollTop: 0 }, 600);
-  return false;
-});
+// $('#home').click(function () {
+//   $("html, body").animate({ scrollTop: 0 }, 600);
+//   return false;
+// });
+
+
 
 // console.log("Large display settings.")
