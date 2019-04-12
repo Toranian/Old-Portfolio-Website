de50378d43e8009d3951578f210dbac11c6f018a
Isaac Morrow <isaacsenseimorrow@gmail.com>
2019-04-11 18:51:11 -0700

    Changed Layout and removed bad code

--- js/main.js
+++ js/main.js
@@ -1,63 +1,35 @@
-// function toggleNav() {
-//   var element = document.getElementById("navbar");
-//   if (element.style.display === "none") {
-//     element.style.display = "block";
-//     // element.style.transform = "translateX(100px)"
-//   }
-//   else {
-//     element.style.display = "none";
-//     // element.style.transform = "translateX(-100px)"
-//
-//   }
-// }
+// Code by Isaac Morrow
+
+//toggles the navigation bar for mobile.
 function toggleNav() {
   var navbar = document.querySelector("#navbar");
   var menu = document.querySelector("#menu");
   var backgroundimage = document.querySelector("#backgroundimage");
 
-  // backgroundimage.classList.toggle("blur")
   menu.classList.toggle("change");
   navbar.classList.toggle("movenav");
 }
 
+// changes the styling after the image is scrolled past.
 $(document).scroll(function() {
     var currentscroll = $(document).scrollTop();
     var distance = $("#about").offset().top;
-    var screen_width = $(window.width());
 
-    if (currentscroll + 20 > distance && screen_width >= 1100) {
-      $(".nav-link").css({
-        "border-bottom":"2px solid black",
-        "border-top":"none",
-        "color":"black",
-      })
-
-      $(".nav-link:hover").css({
-        "background-color":"rgba(0, 0, 0, 0.3)"
-      })
-
-      $("#chevron").css({
-        "border-right":"2px solid black"
-      })
+    if (currentscroll + 10 > distance) {
+      $(".nav-link").addClass("nav-link-new");
+      $("#chevron").hide();
+      $("nav").addClass("navbar-new");
     }
 
     else {
-      $(".nav-link").css({
-        "border-bottom":"2px solid white",
-        "border-top":"2px solid white",
-        "color":"white"
-      })
-
-      $("#chevron").css({
-        "border-right":"2px solid black"
-      })
-
-
+      $(".nav-link").removeClass("nav-link-new");
+      $("#chevron").show();
+      $("nav").removeClass("navbar-new");
     }
 
 })
 
-
+// smooth scrolling.
 $(function() {
   $('a[href^="#"]').on('click', function (e) {
     e.preventDefault();
